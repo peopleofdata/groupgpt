cfd2527bda6720919fb393eb92c3d9e6ccbed19f
Oskar Person <oskar.person@hotmail.com>
2023-05-14 21:57:46 +0200

    Fixed the interjections!

--- app.py
+++ app.py
@@ -8,28 +8,35 @@ from datetime import datetime
 from flask import Flask, request, jsonify, send_from_directory
 from utils import write_to_gsheet, deployment_name, genesis_history, background_info, read_gsheet
 
+#from easylogging import Logger
+#logger = Logger('GroupGPTv0.2.0')
+# turned off to make use of basic Flask logger
+
 app = Flask(__name__, static_folder=".", static_url_path='')
+logger = app.logger
 openai.api_key = os.environ.get('openaikey')
 
-try:
-    history = []
-    table = read_gsheet()
-    print(table)
-    for row in table:
-        history.append(json.loads(row['message']))#{'input': row['user'], 'response': row['assistant']})
-except Exception as e:
-    print(f'Exception in loading history {e}')
-    history = genesis_history
-print(history)
 
-bot_role = """You will reply structured as JSON and only as JSON. \
+history = []
+table = read_gsheet()
+logger.debug(table)
+for row in table:
+    try:
+        history.append({'role':row['role'],'content':row['content']})#{'input': row['user'], 'response': row['assistant']})
+    except Exception as e:
+        logger.error(f'Exception in loading history {e} for row {row}')
+    #history = genesis_history
+logger.debug(history)
+logger.info(f'History has been prepared!')
+
+bot_role = '''You will reply structured as JSON and only as JSON. \
 You shall never provide a reply that is not a JSON. I will feed you a conversation and you will decide\
 whether it is the right time to say something, or not. If the user instructs you to stop talking, you will not\
 respond and set 'should_respond' to 'No'. If you are asked by your name 'Nat' you will always reply. You will 
 provide your response in JSON format and only in the JSON format, with no 
 text outside JSON. A JSON output example looks like this:
-{'should_respond':'Yes', 'response':'Hello world!'}
-The conversation so far:"""
+{"should_respond":"Yes", "response":"Hello world!"}
+The conversation so far:'''
 
 def instruction(background_info):
     return f"{bot_role}"
@@ -40,9 +47,8 @@ now = lambda: datetime.now().strftime("%Y%m%d_%H%M%S")
 def index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/get_history', methods=['GET'])
-def get_history():
-    global history
+def parse_history_for_display(history):
+    '''Construct history for the user, omitting message where should_repond=No'''
     temp_history = []
     print(f"Fetching history from current state: {history}")
     for e in history:
@@ -50,11 +56,21 @@ def get_history():
             if e["role"]=="user":
                 temp_history.append(e)
             elif e['role']=='assistant':
-                temp_history.append(e)
-        except:
-            print(f'/get_history error on fetching rows')
+                content = json.loads(e['content'])
+                print(content, content.keys())
+                if content['should_respond']=='Yes':
+                    temp_history.append({"role":"assistant","content":content['content']})
+                else:
+                    logger.debug(f'parse_history_for_display ommitted assistant message with should_respond!=Yes {e}')
+        except Exception as exception:
+            logger.error(f'/get_history error on fetching row {e} with exception {exception}')
     print(f"Produced temp_history: {temp_history}")
-    return jsonify({"history": temp_history}), 200
+    return temp_history
+
+@app.route('/get_history', methods=['GET'])
+def get_history():
+    global history
+    return jsonify({"history": parse_history_for_display(history)}), 200
 
 @app.route('/store_text', methods=['POST'])
 def complete():
@@ -82,10 +98,10 @@ def complete():
     try:
         print('Writing user...')
         print(history[-1])
-        write_to_gsheet(row = [deployment_name, now(), json.dumps(history[-1]), history[-1]['role'], history[-1]['content']])
+        write_to_gsheet(row = [deployment_name, now(), json.dumps(history[-2]), history[-2]['role'], history[-2]['content']])
         print('Wrote user!')
         print('Writing assistant...')
-        write_to_gsheet(row = [deployment_name, now(), json.dumps(msg), msg['role'], msg['content']])
+        write_to_gsheet(row = [deployment_name, now(), json.dumps(msg), msg['role'], json.dumps(json.loads(msg['content']))])
         print('Wrote assistant!')
     except Exception as e:
         print(f'Sth wrong with writing to gsheet {e}')
